31e3f06df5ec14179821783f20d59fc2f2895b85
Ken <kenediidali@gmail.com>
2023-07-14 07:36:25 +0200

    Mongo DB setup

---
+++ src/db/users.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    auth: {
+        password: {
+            type: String,
+            required: true,
+            select: false
+        },
+        salt: {
+            type: String,
+            required: true,
+            select: false
+        },
+        sessionToken: {
+            type: String,
+            required: true,
+            select: false
+        }
+    }
+})
+
+export const UserModel = mongoose.model('User',UserSchema)
+
+export const getUsers = () => UserModel.find()
+export const getUserByEmail = (email:string) => UserModel.findOne({email})
+export const getUserBySessionToken = (token:string) => UserModel.findOne({'auth.sessionToken':token})
+export const getUserById = (id:string) => UserModel.findById(id)
+export const createUser = (values: Record<string, any>) => new UserModel(values).save().then((user)=> user.toObject())
+export const deleteUserById = (id:string) => UserModel.findOneAndDelete({_id:id})
