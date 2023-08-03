67819426c17d0166d3674250a5c092590f0cdb48
kahassan <kahassan@brochesia.com>
2023-08-03 14:57:07 +0200

    implementing services

--- src/db/workoutPlans/workoutplan.service.ts
+++ src/db/workoutPlans/workoutplan.service.ts
@@ -2,22 +2,19 @@ import {IWorkoutPlan} from "./workoutPlan.interface";
 import WorkoutPlan, {WorkoutPlanDocument} from "./workoutPlan";
 import {WorkoutService} from "../workouts/workout.service";
 import workout, {WorkoutDocument} from "../workouts/workout";
+import {newWorkoutPlan} from "../../controllers/workoutPlan";
 
 export class WorkoutPlanService {
 
-    static async createWorkoutPlan(workoutPlanData: IWorkoutPlan, userId: string):Promise<WorkoutPlanDocument> {
+    static async createWorkoutPlan(workoutPlanData: IWorkoutPlan, userId: string): Promise<WorkoutPlanDocument> {
         return new Promise(async (resolve, reject) => {
             try {
                 const workoutPlan = new WorkoutPlan()
 
-                workoutPlan.name = workoutPlanData.name
-                workoutPlan.frequency = workoutPlanData.frequency
-                workoutPlan.difficulty = workoutPlanData.difficulty
-                workoutPlan.goal = workoutPlanData.goal
-                workoutPlan.duration = workoutPlanData.duration
+                Object.assign(workoutPlan,workoutPlanData)
 
                 for (const workout of workoutPlanData.workouts) {
-                    const upload = await WorkoutService.createWorkout(workout as WorkoutDocument, workoutPlanData.duration,workoutPlan._id)
+                    const upload = await WorkoutService.createWorkout(workout as WorkoutDocument, workoutPlanData.duration, workoutPlan._id)
                     workoutPlan.workouts.push(upload._id)
                 }
 
@@ -33,21 +30,21 @@ export class WorkoutPlanService {
         })
     }
 
-    static async editWorkoutPlan() {
-        return new Promise(async (resolve, reject) => {
-            try{
-
-            }catch (e) {
-                console.log(e)
-                reject(e)
-            }
-        })
-
-    }
-
-    static async getWorkoutPlanById() {
+    static async editWorkoutPlan(id: string, workoutPlan: IWorkoutPlan) {
         return new Promise(async (resolve, reject) => {
             try {
+
+                const workoutPlanToEdit = await WorkoutPlan.findById(id)
+
+                if(workoutPlanToEdit){
+                    Object.assign(workoutPlanToEdit,workoutPlan)
+                } else {
+                    reject(Error("no document with given id"))
+                }
+
+                await workoutPlanToEdit.save()
+
+                resolve(workoutPlanToEdit)
 
             } catch (e) {
                 console.log(e)
@@ -57,10 +54,16 @@ export class WorkoutPlanService {
 
     }
 
-    static async deleteWorkoutPlan() {
+    static async getWorkoutPlanById(id: string) {
         return new Promise(async (resolve, reject) => {
             try {
+                const workoutPlan = await WorkoutPlan.findById(id)
 
+                if (!workoutPlan) {
+                    reject(Error("no document with given id"))
+                }
+
+                resolve(workoutPlan)
             } catch (e) {
                 console.log(e)
                 reject(e)
@@ -69,19 +72,39 @@ export class WorkoutPlanService {
 
     }
 
-    static async getWorkoutPlans() {
+    static async deleteWorkoutPlan(id: string) {
         return new Promise(async (resolve, reject) => {
             try {
-
-
-
-
+                const deleteOperation = await WorkoutPlan.findByIdAndDelete(id)
+                resolve(deleteOperation)
             } catch (e) {
                 console.log(e)
                 reject(e)
             }
         })
+    }
 
+    static async getWorkoutPlans(userId: string): Promise<Array<WorkoutPlanDocument>> {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const workoutPlans = await WorkoutPlan.find({userId: userId})
+                resolve(workoutPlans)
+            } catch (e) {
+                console.log(e)
+                reject(e)
+            }
+        })
+    }
+
+    static async findWorkoutPlanWithProp(prop: any): Promise<WorkoutPlanDocument> {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const document = WorkoutPlan.findOne(prop)
+                resolve(document)
+            } catch (e) {
+                reject(e)
+            }
+        })
     }
 
 }
